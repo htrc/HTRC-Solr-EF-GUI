11f892116f896671c0290076f3a080e2e6078e4d
David Bainbridge <dbbridge@lis-host.lis.illinois.edu>
2017-08-30 06:25:32 -0500

    mock authentication

---
+++ dev/SolrEF-authentication.js
@@ -0,0 +1,20 @@
+function solr_ef_login()
+{
+    htrc_login({
+	"Sign In" : function() {
+
+	    var $iframe_contents = $('#loginFrame').contents();
+	    var username = $iframe_contents.find('#username').val();
+	    var password = $iframe_contents.find('#password').val();
+
+	    if ((username != "")  && (password != "")) {
+		$iframe_contents.find("#error-message").hide();
+		$(this).dialog("close");
+	    }
+	    else {
+		$iframe_contents.find("#error-message").show();
+	    }
+	}
+    });
+    
+}
