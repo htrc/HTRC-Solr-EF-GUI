94eb67c02710271b43683d3af6d6f30725a578de
davidbwaikato <davidb@waikato.ac.nz>
2018-03-08 13:48:45 +1300

    Instructions on how to update the dev(velopment) version to be the prod(uction) version

---
+++ DEV2PROD.sh
@@ -0,0 +1,24 @@
+
+echo "******"
+echo "To update the dev(elopment) version to be the prod(uction) version, do the following:"
+echo ""
+
+echo "  /bin/cp dev/*.* prod/*.*"
+echo "  /bin/cp index-dev.html index.html"
+echo "  /bin/cp page-viewer-dev.html page-viewer.html"
+echo ""
+
+echo "Open index.html, and:"
+echo "   replace all dev/ links to prod/"
+echo "   change the link at the bottom to give access to the bleeding edge version (<!-- -->)"
+echo "Open prod/SolrEF-result-set, and:"
+echo "   change 'page-viewer-dev.html' to 'page-viewer.html'"
+echo ""
+
+echo "Now run:"
+echo ""
+echo "  git status"
+echo ""
+echo "to see if what needs to be committed.  Note, there may be some new files to add"
+
+echo "******"
