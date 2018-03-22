4bd7feb0d368bba3ea3ab0d8e201b5bf2fdcd4c7
davidbwaikato <davidb@waikato.ac.nz>
2018-03-22 22:16:38 +1300

    Evolution of script

--- CONVERT-TO-JSX.sh
+++ CONVERT-TO-JSX.sh
@@ -4,10 +4,19 @@
 
 cat index-dev-body.html | sed '/<!--.*-->/d' | sed '/<!--/,/-->/d' > /tmp/no-comments.xml
 
-cat /tmp/no-comments.xml | sed 's/class=/className=/g' > /tmp/fixed-class.xml
+cat /tmp/no-comments.xml \
+  | sed 's/dev\//HTRC-Solr-EF-GUI\/dev\//g' \
+  | sed 's/class=/className=/g' \
+  | sed 's/&nbsp;/ANDNBSP/g' > /tmp/fixed-class.xml
 
 # cat /tmp/fixed-class.xml | sed 's/style="\([^"]*\)"/style={{\1}}/g' | sed 's/{{\([^-]+\)-\(.\)\([^}]+\)}}/{{\1\U\2\3}}/g'
 
-cat /tmp/fixed-class.xml | ./css-to-jsx.pl | sed 's/(//g' | sed 's/)//g' 
+#xmllint --format /tmp/fixed-class.xml | ./css-to-jsx.pl | sed 's/ANDNBSP/&nbsp;/g' > index-dev-body.jsx
+cat /tmp/fixed-class.xml | ./css-to-jsx.pl | sed 's/ANDNBSP/&nbsp;/g' > index-dev-body.jsx
+
+
+egrep "script|link" index-dev-header.html \
+ | sed 's/dev\//HTRC-Solr-EF-GUI\/dev\//g' > link-script.jsx
+
 
 
