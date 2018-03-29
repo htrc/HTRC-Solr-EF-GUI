5039c8d257b1d35bd7babed6cbed77b918feebcd
davidbwaikato <davidb@waikato.ac.nz>
2018-03-29 13:53:27 +1300

    Further tweaking to better produce what is needed for JSX files

--- CONVERT-TO-JSX.sh
+++ CONVERT-TO-JSX.sh
@@ -11,12 +11,14 @@ cat /tmp/no-comments.xml \
 
 # cat /tmp/fixed-class.xml | sed 's/style="\([^"]*\)"/style={{\1}}/g' | sed 's/{{\([^-]+\)-\(.\)\([^}]+\)}}/{{\1\U\2\3}}/g'
 
-#xmllint --format /tmp/fixed-class.xml | ./css-to-jsx.pl | sed 's/ANDNBSP/&nbsp;/g' > index-dev-body.jsx
-cat /tmp/fixed-class.xml | ./css-to-jsx.pl | sed 's/ANDNBSP/&nbsp;/g' > index-dev-body.jsx
+xmllint --format /tmp/fixed-class.xml | ./css-to-jsx.pl | sed 's/ANDNBSP/&nbsp;/g' > index-dev-body.jsx
+#cat /tmp/fixed-class.xml | ./css-to-jsx.pl | sed 's/ANDNBSP/&nbsp;/g' > index-dev-body.jsx
 
 
 egrep "script|link" index-dev-header.html \
- | sed 's/dev\//HTRC-Solr-EF-GUI\/dev\//g' > link-script.jsx
+ | sed 's/assets\//assets\/HTRC-Solr-EF-GUI\/assets\//g' \
+ | sed 's/dev\//assets\/HTRC-Solr-EF-GUI\/dev\//g' \
+ > link-script.jsx
 
 
 
