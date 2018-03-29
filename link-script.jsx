052d5bdd4c4418fb9863f8444d09e4bb0f18d1ad
davidbwaikato <davidb@waikato.ac.nz>
2018-03-29 13:52:34 +1300

    To make inclusion in SolrEFPage.jsx easier

---
+++ link-script.jsx
@@ -0,0 +1,39 @@
+        <link rel="shortcut icon" href="https://analytics.hathitrust.org/assets/HTRC-Solr-EF-GUI/assets/images/favicon.ico">
+        <link href="assets/HTRC-Solr-EF-GUI/assets/bootstrap.css" rel="stylesheet" media="screen">
+        <link href="assets/HTRC-Solr-EF-GUI/assets/main.css" rel="stylesheet" media="screen">
+        <link href="assets/HTRC-Solr-EF-GUI/assets/flat.css" rel="stylesheet" media="screen">
+        <link href="assets/HTRC-Solr-EF-GUI/assets/font-awesome.css" rel="stylesheet">
+        <link href="assets/HTRC-Solr-EF-GUI/assets/tomorrow.css" rel="stylesheet">
+	<link href="assets/HTRC-Solr-EF-GUI/assets/jquery-ui-lightness-1.12.1/jquery-ui.min.css" rel="stylesheet">
+	<link href="assets/HTRC-Solr-EF-GUI/assets/paging.css" rel="stylesheet">
+        <link href="assets/HTRC-Solr-EF-GUI/dev/SolrEF.css" rel="stylesheet">
+        <link href="assets/HTRC-Solr-EF-GUI/dev/SolrEF-progressbar.css" rel="stylesheet">
+        <script src="assets/HTRC-Solr-EF-GUI/assets/jquery.js"></script>
+        <script src="assets/HTRC-Solr-EF-GUI/assets/highlight.js"></script>
+        <script src="assets/HTRC-Solr-EF-GUI/assets/stupidtable.js"></script>
+        <script src="assets/HTRC-Solr-EF-GUI/assets/bootstrap.js"></script>
+        <script src="assets/HTRC-Solr-EF-GUI/assets/bowser.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/assets/download.min.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/assets/lucene-query-parser.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/assets/jquery-ui-lightness-1.12.1/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="assets/HTRC-Solr-EF-GUI/assets/htrcwarnings.js"></script>
+        <script type="text/javascript" src="assets/HTRC-Solr-EF-GUI/assets/uploadws.js"></script>
+        <script type="text/javascript" src="assets/HTRC-Solr-EF-GUI/assets/ga-download-tracking.js"></script>
+	<script type="text/javascript" src="assets/HTRC-Solr-EF-GUI/assets/paging.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/iso-639-1.js"></script> <!-- defines var 'isoLangs' -->
+	<script src="assets/HTRC-Solr-EF-GUI/dev/lookup-vars.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/SolrEF-globals.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/SolrEF-settings.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/SolrEF-utils.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/SolrEF-authentication.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/SolrEF-iprogressbar.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/SolrEF-worksets.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/SolrEF-stream.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/SolrEF-explain.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/SolrEF-facet-filter.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/SolrEF-shoppingcart.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/SolrEF-query-breakdown.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/SolrEF.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/SolrEF-result-set.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/SolrEF-autocomplete.js"></script>
+	<script src="assets/HTRC-Solr-EF-GUI/dev/SolrEF-dom-ready.js"></script>
