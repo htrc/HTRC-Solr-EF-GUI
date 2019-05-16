072f7ab198306f92ee5d2c25ec59e5238ad15103
David Bainbridge <davidb@waikato.ac.nz>
2019-05-16 03:42:17 -0500

    Moving on to testing at 2000 and 1000 level of term frequency

--- performance-testing/batch-test.pl
+++ performance-testing/batch-test.pl
@@ -23,27 +23,14 @@ if ($exptname =~ m@^(.*)/@) {
 
 my $word_freq = batch::read_in_word_frequencies("en-word-freq-top-10000.txt");
 
-#my $title_sherlock_data_file = "$exptname-titleSherlock-$data_tail";
-#wramp_batch_test($title_sherlock_data_file,5,1,["title_t:sherlock"]);
-
-#my $entext_violins_data_file = "$exptname-entextViolins-$data_tail";
-#wramp_batch_test($entext_violins_data_file,10,1,["en_htrctokentext:violins"]);
-
-#my $entext_violin_data_file = "$exptname-entextViolin-$data_tail";
-#wramp_batch_test($entext_violin_data_file,10,1,["en_htrctokentext:violin"]);
- 
-
-my $rand_query_slice7000 = batch::generate_slice($word_freq,7000);
-my $entext_slice7000_data_file = "$exptname-randSlice7000-$data_tail";
-batch::wramp_batch_test($entext_slice7000_data_file,5,1,$rand_query_slice7000);
-
-my $rand_query_slice5000 = batch::generate_slice($word_freq,5000);
-my $entext_slice5000_data_file = "$exptname-randSlice5000-$data_tail";
-batch::wramp_batch_test($entext_slice5000_data_file,10,1,$rand_query_slice5000);
-
-my $rand_query_slice3000 = batch::generate_slice($word_freq,3000);
+my $rand_query_slice3000 = batch::generate_slice($word_freq,2000);
 my $entext_slice3000_data_file = "$exptname-randSlice3000-$data_tail";
-batch::wramp_batch_test($entext_slice3000_data_file,25,2,$rand_query_slice3000);
+batch::wramp_batch_test($entext_slice3000_data_file,30,2,$rand_query_slice3000);
+
+my $rand_query_slice3000 = batch::generate_slice($word_freq,1000);
+my $entext_slice3000_data_file = "$exptname-randSlice3000-$data_tail";
+batch::wramp_batch_test($entext_slice3000_data_file,30,2,$rand_query_slice3000);
+
 
 if (defined $exptdir) {
     print "Now move 'json-output' into your '$exptdir' area:\n\n";
