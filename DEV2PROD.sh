8a7494ca9a7fcaeccb57be445488e92d83567bad
David Bainbridge <davidb@waikato.ac.nz>
2019-09-03 21:06:02 -0500

    Updated to reflect new index-body-dl-dev.html file.  Also changed so be run in a way that actually runs the command, rather than relying on copy-and-paste

--- DEV2PROD.sh
+++ DEV2PROD.sh
@@ -1,21 +1,46 @@
 
-echo "******"
-echo "To update the dev(elopment) version to be the prod(uction) version, do the following:"
-echo ""
 
-echo "  /bin/cp dev/*.* prod/."
-echo "  /bin/cp index-dev.html index.html"
-echo "  /bin/cp json-page-viewer-dev.html json-page-viewer.html"
-echo ""
+run_cmd=0;
 
-#echo "Open index.html, and:"
-#echo "   replace all dev/ links to prod/"
-#echo "   change the link at the bottom to give access to the bleeding edge version (<!-- -->)"
-#echo "Open prod/SolrEF-result-set.json, and:"
-#echo "   change 'page-viewer-dev.html' to 'page-viewer.html'"
-#echo ""
+if [ "x$1" = "-f" ] ; then
+    run_cmd=1
+elif [ "x$1" = "-force" ] ; then
+    run_cmd=1
+elif [ "x$1" = "--force" ] ; then
+    run_cmd=1
+fi
 
-echo "Now run:"
+
+cmd1='/bin/cp dev/*.* prod/.'
+cmd2='/bin/cp index-body-dl-dev.html index-body-dl.html'
+cmd3='/bin/cp index-dev.html index.html'
+cmd4='/bin/cp json-page-viewer-dev.html json-page-viewer.html'
+
+if [ $run_cmd = 1 ] ; then
+    echo "Updating production version to be the same as the development version"
+    $cmd1 && $cmd2 && $cmd3 && $cmd4
+    if [ $? != 0 ] ; then
+	echo -e "Error encountered running:\n   $cmd1 && $cmd2 && $cmd3 && $cmd4" 1>&2
+	exit 1
+    fi
+else
+
+    echo "******"
+    echo "To update the dev(elopment) version to be the prod(uction) version, do the following:"
+    echo ""
+
+    echo "  $cmd1"
+    echo "  $cmd2"
+    echo "  $cmd3"
+    echo "  $cmd4"
+    echo ""
+    
+    echo "Alternatively, run: "
+    echo "  ./DEV2PROD.sh --force"
+    echo ""
+fi
+
+echo "Not every file will have necessarily been changed. Run:"
 echo ""
 echo "  git status"
 echo ""
