782d927e745dc8d17ee64697630884178b08f5dc
David Bainbridge <davidb@waikato.ac.nz>
2019-10-24 16:40:09 -0500

    Prod brought up to date

--- prod/SolrEF-settings.js
+++ prod/SolrEF-settings.js
@@ -124,3 +124,12 @@ $.ajax({
     }
 });
 
+
+if (!String.prototype.startsWith) {
+    Object.defineProperty(String.prototype, 'startsWith', {
+	value: function(search, rawPos) {
+	    var pos = rawPos > 0 ? rawPos|0 : 0;
+	    return this.substring(pos, pos + search.length) === search;
+	}
+    });
+}
